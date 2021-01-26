c075fc6fcc4ea389d197c95392d84728d3a30fb9
Utkarsh87 <f20180368@pilani.bits-pilani.ac.in>
2021-01-27 00:54:51 +0530

    add gif functionality

--- src/simple_carver.py
+++ src/simple_carver.py
@@ -4,77 +4,48 @@ Author: Utkarsh Kumar Singh
 Most of the code has been replicated from https://github.com/dtfiedler/seam-carving-python
 """
 
-
-import numpy as np
-import cv2
+import os
 import sys
-from scipy.ndimage.filters import convolve
-from tqdm import trange # progress bar
 import warnings
 warnings.filterwarnings("ignore")
 
+import cv2
+import numpy as np
+from tqdm import trange # progress bar
+
+from giffer import GIFMake
+
+results_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), "results/")
+images_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), "images/")
+image_list = []
+
 def get_energy_map(img):
-    img = img.astype(np.uint8)
     # use the gradient magnitude function to generate the energy map
+    img = img.astype(np.uint8)
+
     # apply gaussian smoothing
     img = cv2.GaussianBlur(img, (3, 3), 0).astype(np.int8)
+
     # turn into greyscale
     grey = cv2.cvtColor(img.astype(np.uint8), cv2.COLOR_BGR2GRAY)
+
     # use sobel filters(3*3) to get gradients
     sobel_x = cv2.Sobel(grey, cv2.CV_64F, 1, 0, ksize=3)
     sobel_y = cv2.Sobel(grey, cv2.CV_64F, 0, 1, ksize=3)
     abs_sobel_x = cv2.convertScaleAbs(sobel_x)
     abs_sobel_y = cv2.convertScaleAbs(sobel_y)
+
     # merge into energy map
     energy_map = cv2.addWeighted(abs_sobel_x, 0.5, abs_sobel_y, 0.5, 0)
+
     # write color map
     color_map = cv2.applyColorMap(energy_map, cv2.COLORMAP_JET)
-    cv2.imwrite("colormap.jpg", color_map)
+    cv2.imwrite(results_dir+"colormap.jpg", color_map)
+
     # write energy map
-    cv2.imwrite("energy.jpg", energy_map)
+    cv2.imwrite(results_dir+"energy.jpg", energy_map)
 
     return energy_map
-
-# # compute the enery map of the image
-# def calc_energy(img):
-#     # E = Ex + Ey   (E: energy of a pixel)
-#     # Ex = deltaRx^2 + deltaGx^2 + deltaBx^2
-#     # Ey = deltaRy^2 + deltaGy^2 + deltaBy^2
-
-#     img = img.astype(np.uint8)
-#     # use the gradient magnitude function to generate the energy map
-#     # apply gaussian smoothing
-#     img = cv2.GaussianBlur(img, (3, 3), 0).astype(np.int8)
-#     # turn into greyscale
-#     grey_img = cv2.cvtColor(img.astype(np.uint8), cv2.COLOR_BGR2GRAY)
-
-#     # use Sobel filters for this computation
-#     # create 2D Sobel filters and stack 3 for each colour channel
-#     hor_filter = np.array([
-#             [1.0, 2.0, 1.0],
-#             [0.0, 0.0, 0.0],
-#             [-1.0, -2.0, -1.0],
-#     ])
-#     # hor_filter = np.stack([hor_filter]*3, axis=2) # 2D -> 3D filter
-
-#     ver_filter = np.array([
-#         [1.0, 0.0, -1.0],
-#         [2.0, 0.0, -2.0],
-#         [1.0, 0.0, -1.0],
-#     ])
-#     # ver_filter = np.stack([ver_filter]*3, axis=2)
-
-#     conv_out = np.absolute(convolve(grey_img, hor_filter)) + np.absolute(convolve(grey_img, ver_filter)) # E = Ex + Ey
-#     # energy_map = conv_out.sum(axis=2) # sum gradients for each channel
-#     energy_map = conv_out
-
-#     # write color map
-#     color_map = cv2.applyColorMap(energy_map, cv2.COLORMAP_JET)
-#     cv2.imwrite("colormap.jpg", color_map)
-
-#     cv2.imwrite("energy.jpg", energy_map)
-#     return energy_map
-
 
 def delete_seam(img, seam):
     rows, cols, _ = img.shape
@@ -86,11 +57,11 @@ def delete_seam(img, seam):
     return output_img
 
 def overlay_seam(seam, file):
-    img = cv2.imread(file)
+    img = cv2.imread(results_dir+file)
     img_with_seam = np.copy(img)
     x, y = np.transpose([(i, int(j)) for i, j in enumerate(seam)])
     img_with_seam[x, y] = (0, 0, 255)
-    cv2.imwrite(file, img_with_seam)
+    cv2.imwrite(results_dir+file, img_with_seam)
 
 def get_min_seam(img, energy_map):
     rows, cols, _ = img.shape
@@ -129,20 +100,33 @@ def get_min_seam(img, energy_map):
     return seam
 
 def carve_seams(output_img):
-    num_seams = 10
+    num_seams = 150 # how many seams to remove from the image
     energy_map = get_energy_map(output_img)
 
     for _ in trange(num_seams):
         seam = get_min_seam(output_img, energy_map)
-        overlay_seam(seam, "castle_with_seams.jpg")
+
+        overlay_seam(seam, "castle_with_seams.jpg") # to show all seams removed overlayed on the original image
+
+        cv2.imwrite(results_dir+"cur_castle_with_next_seam.jpg", output_img)
+        overlay_seam(seam, "cur_castle_with_next_seam.jpg") # overlay seam on current image(used for making gif)
+        img_for_gif = cv2.imread(results_dir+"cur_castle_with_next_seam.jpg")
+        image_list.append(img_for_gif)
+
         output_img = delete_seam(output_img, seam)
-        energy_map = get_energy_map(output_img)
+        image_list.append(output_img)
 
-    cv2.imwrite("castle_new.jpg", output_img)
+        energy_map = get_energy_map(output_img) # get energy map of new image
 
-if __name__ == "__main__":
-    img = cv2.imread("castle.jpg")
-    output_img = np.copy(img)
-    cv2.imwrite("castle_with_seams.jpg", img)
+    cv2.imwrite(results_dir+"castle_new.jpg", output_img)
 
-    carve_seams(output_img)
+img = cv2.imread(images_dir+"castle.jpg")
+output_img = np.copy(img)
+cv2.imwrite(results_dir+"castle_new.jpg", output_img)
+cv2.imwrite(results_dir+"castle_with_seams.jpg", img)
+carve_seams(output_img)
+
+# create gif
+run = GIFMake(image_list)
+run.save_imgs()
+run.gif_make()
